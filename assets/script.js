af83a640ed28d8469029db4733ac1b6a83199ad4
Akihiro MATOBA <matobaa+github@gmail.com>
2022-10-23 01:15:06 +0900

    WIP add navbar legend timeslider

--- assets/script.js
+++ assets/script.js
@@ -48,6 +48,37 @@ async function main() {
         }
     }).addTo(map);
     // TODO
+    L.control.scale({ maxWidth: 200, position: 'topleft', imperial: false }).addTo(map);
+
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function (map) {
+        const div = L.DomUtil.create('div', 'info legend');
+        div.innerHTML =
+            `<div id="legend"><span class="title">NHK記事数</span><br/>`
+            + `<span class="note">NHKが公開した災害に関する記事の数です。</span>`
+            + `<div style="opacity: 50%">~ 50</div>`
+            + `<div style="opacity: 30%">~ 30</div>`
+            + `<div style="opacity: 20%">~ 20</div>`
+            + `<div style="opacity: 10%">~ 10</div>`
+            + `<div style="opacity:  5%">~ 5</div>`
+            + `</div>`
+        return div;
+    };
+    legend.addTo(map);
+
+    var timeslider = L.control({ position: 'bottomleft' });
+    timeslider.onAdd = function (map) {
+        const div = L.DomUtil.create('div', 'info timeslider');
+        div.innerHTML = `<input id="timeslider" type="range" style="width: 500px"><label for="timeslider">time</label>`;
+        L.DomEvent.on(div, 'click', L.DomEvent.stopPropagation);
+        L.DomEvent.on(div, 'dblclick', L.DomEvent.stopPropagation);
+        L.DomEvent.on(div, 'mousemove', L.DomEvent.stopPropagation);
+        L.DomEvent.on(div, 'mousewheel', L.DomEvent.stopPropagation);
+        return div;
+    };
+    timeslider.addTo(map);
+
+    // L.divOverlay({}).addTo(map);
 }
 
 document.addEventListener('DOMContentLoaded', main);
