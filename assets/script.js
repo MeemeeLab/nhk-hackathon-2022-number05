91483f65a98ce0a68b6865a6bba5fa5ac8370c6e
MeemeeLab <57629983+MeemeeLab@users.noreply.github.com>
2022-10-22 16:20:13 +0900

    Added map

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
 function main() {
+    const map = L.map('map').setView([36.2048, 138.2529], 6);
+    L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://gsi.go.jp/">地理院タイル</a>'
+    }).addTo(map);
     // TODO
 }
 
